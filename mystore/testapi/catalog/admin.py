be9c4a537ee04ba1f80333d38b3e40c5121c6b1a
kolomoetsv <52840884+kolomoetsv@users.noreply.github.com>
2021-05-17 01:45:53 +0300

    - added author and book models - configured display of lists and added filters in backoffice

--- mystore/testapi/catalog/admin.py
+++ mystore/testapi/catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Book, Author
+
 
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fieldsets = (
+        (None, {
+            'fields': ('first_name', 'last_name')
+        }),
+        ('Years of life', {
+            'fields': ('date_of_birth', 'date_of_death')
+        }),
+    )
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre')
+    list_filter = ('author', 'genre')
--- mystore/testapi/catalog/models.py
+++ mystore/testapi/catalog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Book(models.Model):
+
+    BOOK_GENRES = (
+        ('action_adventure', 'Action and Adventure'),
+        ('detective', 'Detective'),
+        ('fantasy', 'Fantasy'),
+        ('hist_fic', 'Historical Fiction'),
+        ('horror', 'Horror'),
+        ('romance', 'Romance'),
+        ('sci_fi', 'Sci-Fi'),
+    )
+
+    title = models.CharField(max_length=200, verbose_name='Book title')
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, verbose_name='Author of the book')
+    genre = models.CharField(max_length=20, choices=BOOK_GENRES, help_text='Select a genre for this book')
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.title
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular book instance.
+        """
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='Authors name')
+    last_name = models.CharField(max_length=100, verbose_name='Authors surname')
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular author instance.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return '{0},{1}'.format(self.last_name, self.first_name)
