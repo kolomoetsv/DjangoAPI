bdf21bb1b29e53463cfe111388e4132f30373cb6
kolomoetsv <52840884+kolomoetsv@users.noreply.github.com>
2021-05-19 01:40:57 +0300

    added get, patch, delete methods

--- mystore/testapi/catalog/api/api_views.py
+++ mystore/testapi/catalog/api/api_views.py
@@ -1,5 +1,6 @@
 from django.views import View
 from django.http import JsonResponse
+from django.core import serializers
 import json
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -10,8 +11,11 @@ from ..models import Book
 @method_decorator(csrf_exempt, name='dispatch')
 class BookAPIView(View):
     def post(self, request):
+        # Decode and parse the incoming request's body into an object we can work with
         data = json.loads(request.body.decode("utf-8"))
 
+        # Persist a Book to our database, via the create() method of the Model class,
+        # filling it with our extracted data.
         book = Book.objects.create(
             title=data.get('title'),
             author=data.get('author'),
@@ -21,4 +25,46 @@ class BookAPIView(View):
         data = {
             "message": 'New book added to Books with id: {}'.format(book.id)
         }
+        # Convert Python dictionary to a valid JSON object that is sent over HTTP back to the client.
+        # Set the status code to 201 to signify resource creation on the server-end.
         return JsonResponse(data, status=201)
+
+    def get(self, request):
+        books_data = serializers.serialize("json", Book.objects.all())
+        books_count = Book.objects.count()
+
+        data = {
+            'items': books_data,
+            'count': books_count,
+        }
+
+        return JsonResponse(data, safe=False)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class BookAPIUpdateView(View):
+    def patch(self, request, book_id):
+        data = json.loads(request.body.decode("utf-8"))
+        book = Book.objects.get(id=book_id)
+        book.author = data['author']
+        book.save()
+
+        new_record = Book.objects.filter(id=book_id)
+        body = serializers.serialize('json', new_record)
+
+        data = {
+            'book': body,
+            'message': 'Book {} has been updated'.format(book_id)
+        }
+
+        return JsonResponse(data)
+
+    def delete(self, request, book_id):
+        book = Book.objects.get(id=book_id)
+        book.delete()
+
+        data = {
+            'message': 'Book {} has been deleted'.format(book.id)
+        }
+
+        return JsonResponse(data)
--- mystore/testapi/catalog/api/urls.py
+++ mystore/testapi/catalog/api/urls.py
@@ -3,4 +3,5 @@ from . import api_views
 
 urlpatterns = [
     path('books/', api_views.BookAPIView.as_view()),
+    path('update-book/<int:book_id>', api_views.BookAPIUpdateView.as_view()),
 ]
