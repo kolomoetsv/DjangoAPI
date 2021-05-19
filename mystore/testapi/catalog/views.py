59a720fece279606a111ddad88ff01d3789513c8
kolomoetsv <52840884+kolomoetsv@users.noreply.github.com>
2021-05-19 20:53:54 +0300

    added pagination for book list view

--- mystore/testapi/catalog/views.py
+++ mystore/testapi/catalog/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
+from collections import OrderedDict
+
+from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 
 from .serializers import BookSerializer
 from .models import Book
 
 
+class BookPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('books_count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('books', data)
+        ]))
+
+
 class BookListAPIView(ListAPIView):
 
     serializer_class = BookSerializer
+    pagination_class = BookPagination
     queryset = Book.objects.all()
     filter_backends = [SearchFilter]
     search_fields = ['title', 'author', 'genre']
